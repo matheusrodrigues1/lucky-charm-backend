1d97308999fd2ba1fd1d55dce7bbaec8dbded265
matheusrodrigues1 <matech00@outlook.com>
2023-06-14 21:17:38 -0300

    update deployy

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
 
+const port = process.env.PORT || 3000;
 const prisma = new PrismaClient();
 const app = express();
 
